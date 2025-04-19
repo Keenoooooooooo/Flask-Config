b31862a9f9ef23aeee52f0539f03e54a22ea5c56
raprealin <86905458+Keenoooooooooo@users.noreply.github.com>
2025-04-19 22:04:54 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask #--> this imports the flask module 
 
 app = Flask(__name__)
 @app.route('/')
@@ -7,3 +7,6 @@ def home():
     return "Hello World!"
 if __name__ == '__main__':
     app.run()
+
+#note: you may have a hard time executing this code on your localhost.
+    #Try manually typing "localhost:5000" in your local browser instead, make sure that the Python file is already running.   
